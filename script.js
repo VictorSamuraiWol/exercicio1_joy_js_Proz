2cc5d423f14fb4486aa7520e9429e26d3e63e68b
Victor Cardoso <victorclmatos@gmail.com>
2023-10-13 15:31:49 -0300

    execução 3x

--- script.js
+++ script.js
@@ -8,8 +8,16 @@ while (i<3) {
 /*usando função e for*/
 const conexao = () => {
     for (i=1;i<4;i++) {
-        console.log ("Conexão feitao com sucesso!", i);
+        console.log ("Conexão feita com sucesso!", i);
     }
 }
-
 conexao();
+
+/*executando uma função 3x*/
+const conexao3x = () => {
+    console.log("Conexão feita com sucesso!")
+}
+
+conexao3x();
+conexao3x();
+conexao3x();
